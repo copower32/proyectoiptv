72eae0a8e9c93b79295b11190cc458f30b00c576
copower32 <lili.ana.blanco@hotmail.com>
2024-12-06 17:50:35 -0500

    añadida tarjeta de promocion

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -5,16 +5,18 @@ const Plans: React.FC = () => {
 
   const plans = {
     one: [
-      { duration: "1 Mes", oldPrice: "€14.99", price: "€9.99", features: ["+36.000 Canales en vivo", "+150.000 Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7","Tecnología anticongelante."] },
-      { duration: "3 Meses", oldPrice: "€29.99", price: "€24.99", features: ["+36.000 Canales en vivo", "+150.000 Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7","Tecnología anticongelante."] },
-      { duration: "6 Meses", oldPrice: "€44.99", price: "€39.99", features: ["+36.000 Canales en vivo", "+150.0000 Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7","Tecnología anticongelante."] },
-      { duration: "12 Meses", oldPrice: "€69.99", price: "€64.99", features: ["+36.000 Canales en vivo", "+150.0000 Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7","Tecnología anticongelante."] },
+      { duration: "6 horas ", oldPrice: "", price: "¡Gratis!", features: ["+36.000 Canales en vivo", "+150.000 Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7", "Tecnología anticongelante."], isFree: true },
+      { duration: "1 Mes", oldPrice: "€14.99", price: "€9.99", features: ["+36.000 Canales en vivo", "+150.000 Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7", "Tecnología anticongelante."] , isFree: false },
+      { duration: "3 Meses", oldPrice: "€29.99", price: "€24.99", features: ["+36.000 Canales en vivo", "+150.000 Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7", "Tecnología anticongelante."], isFree: false },
+      { duration: "6 Meses", oldPrice: "€44.99", price: "€39.99", features: ["+36.000 Canales en vivo", "+150.0000 Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7", "Tecnología anticongelante."], isFree: false },
+      { duration: "12 Meses", oldPrice: "€69.99", price: "€64.99", features: ["+36.000 Canales en vivo", "+150.0000 Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7", "Tecnología anticongelante."] },
+      
     ],
     two: [
-      { duration: "1 Mes", oldPrice: "€22.99", price: "€17.99", features: ["+36.000 Canales en vivo", "+150.000 Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7","Tecnología anticongelante."] },
-      { duration: "3 Meses", oldPrice: "€49.99", price: "€44.99", features: ["+36.000 Canales en vivo", "+150.000 Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7","Tecnología anticongelante."] },
-      { duration: "6 Meses", oldPrice: "€79.99", price: "€74.99", features: ["+36.000 Canales en vivo", "+150.000Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7","Tecnología anticongelante."] },
-      { duration: "12 Meses", oldPrice: "€124.99", price: "€119.99", features: ["+36.000 Canales en vivo", "+150.0000 Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7","Tecnología anticongelante."] },
+      { duration: "1 Mes", oldPrice: "€22.99", price: "€17.99", features: ["+36.000 Canales en vivo", "+150.000 Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7", "Tecnología anticongelante."], isFree: false },
+      { duration: "3 Meses", oldPrice: "€49.99", price: "€44.99", features: ["+36.000 Canales en vivo", "+150.000 Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7", "Tecnología anticongelante."], isFree: false },
+      { duration: "6 Meses", oldPrice: "€79.99", price: "€74.99", features: ["+36.000 Canales en vivo", "+150.000Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7", "Tecnología anticongelante."], isFree: false },
+      { duration: "12 Meses", oldPrice: "€124.99", price: "€119.99", features: ["+36.000 Canales en vivo", "+150.0000 Películas Y Series", "Cambiar entre dispositivos", "Uso de VPN compatible", "Calidad 4k/HD/UHD", "Guía de televisión", "Mira en cualquier dispositivo", "Soporte VIP gratuito 24/7", "Tecnología anticongelante."], isFree: false },
     ],
   };
 
@@ -26,39 +28,38 @@ const Plans: React.FC = () => {
         <div className="flex justify-center mb-8">
           <button
             onClick={() => setDeviceOption("one")}
-            className={`py-2 px-4 rounded-l-lg ${
-              deviceOption === "one" ? "bg-primary text-white" : "bg-gray-700 text-gray-300"
-            }`}
+            className={`py-2 px-4 rounded-l-lg ${deviceOption === "one" ? "bg-primary text-white" : "bg-gray-700 text-gray-300"
+              }`}
           >
             Un dispositivo
           </button>
           <button
             onClick={() => setDeviceOption("two")}
-            className={`py-2 px-4 rounded-r-lg ${
-              deviceOption === "two" ? "bg-primary text-white" : "bg-gray-700 text-gray-300"
-            }`}
+            className={`py-2 px-4 rounded-r-lg ${deviceOption === "two" ? "bg-primary text-white" : "bg-gray-700 text-gray-300"
+              }`}
           >
             Dos dispositivos
           </button>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className={`grid gap-6 ${deviceOption === "one" ? "grid-cols-1 sm:grid-cols-2 lg:grid-cols-5" : "grid-cols-1 sm:grid-cols-2 lg:grid-cols-4"}`}>
           {plans[deviceOption].map((plan, index) => (
             <div
               key={index}
-              className="bg-white text-black p-6 rounded-lg shadow-lg mx-auto sm:w-4/5 lg:w-auto"
+              className={`p-6 rounded-lg shadow-lg mx-auto sm:w-4/5 lg:w-auto ${plan.isFree ? "bg-red-700    text-white" : "bg-white text-black"}`}
             >
               <h3 className="text-2xl font-bold mb-2">{plan.duration}</h3>
-              <p className="text-gray-500 line-through">{plan.oldPrice}</p>
+              {plan.oldPrice && <p className="text-gray-500 line-through">{plan.oldPrice}</p>}
               <p className="text-3xl font-bold mb-4">{plan.price}</p>
               <div className="flex justify-center">
                 <a
-                  href={`https://wa.me/34624063991?text=Hola,%20estoy%20interesado%20en%20el%20plan%20de%20${plan.duration}%20por%20${plan.price}`}
+                  href={`https://wa.me/34624063991?text=Hola,%20estoy%20interesado%20en%20${plan.isFree ? "probar el plan de Prueba gratuita" : `el plan de ${plan.duration} por ${plan.price}`}`}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="bg-primary text-white py-3 px-8 rounded-full text-lg shadow-lg hover:bg-red-600 transition-all"
+                  className={`py-3 px-8 rounded-full text-lg shadow-lg transition-all ${plan.isFree ? "bg-white text-black hover:bg-gray-200" : "bg-primary text-white hover:bg-red-600"
+                    }`}
                 >
-                  Comprar IPTV
+                  {plan.isFree ? "Solicitar prueba" : "Comprar IPTV"}
                 </a>
               </div>
               <ul className="mt-4 text-left space-y-2">
